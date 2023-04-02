aa94c3c579563cb8ac98202d6b1e04a92f9c92be
plr7edu <104339100+plr7edu@users.noreply.github.com>
2023-04-02 19:57:05 +0530

    Add Easy Worship 7 Configs

--- win-dotfiles-backup.bat
+++ win-dotfiles-backup.bat
@@ -20,6 +20,10 @@ xcopy /y /E "C:\Users\plr\AppData\Roaming\geany\*" "C:\Users\plr\Documents\GIT-R
 ::Espanso
 xcopy /y /E "C:\Users\plr\AppData\Roaming\espanso\*" "C:\Users\plr\Documents\GIT-REPO\windows-dotfiles\espanso\"
 
+::Essy Worship 7
+xcopy /y /E "C:\ProgramData\Softouch\*" "C:\Users\plr\Documents\GIT-REPO\windows-dotfiles\easyworship7\programdata\"
+xcopy /y /E "C:\Users\plr\AppData\Roaming\Softouch\*" "C:\Users\plr\Documents\GIT-REPO\windows-dotfiles\easyworship7\roaming\"
+
 ::Create a Text file with current date and time
 
 @echo off
