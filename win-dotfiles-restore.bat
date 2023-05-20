c2f5fb41e69ab5719a9dd42435462629909b863b
plr7edu <104339100+plr7edu@users.noreply.github.com>
2023-05-20 08:46:55 +0530

    Repo Update: 2023-05-20 08:46:52

--- win-dotfiles-restore.bat
+++ win-dotfiles-restore.bat
@@ -1,26 +1,26 @@
 
 ::Alaritty
-xcopy "C:\Users\plr\Documents\GIT-REPO\windows-dotfiles\alacritty\" "C:\Users\plr\AppData\Roaming\alacritty\" 
+xcopy "C:\Users\PLR\Documents\MY-GIT-REPO\windows-dotfiles\alacritty\" "C:\Users\PLR\AppData\Roaming\alacritty\" 
 
 ::Starship
-xcopy "C:\Users\plr\Documents\GIT-REPO\windows-dotfiles\starship\" "C:\Users\plr\AppData\Roaming\starship\"
+xcopy "C:\Users\PLR\Documents\MY-GIT-REPO\windows-dotfiles\starship\" "C:\Users\PLR\AppData\Roaming\starship\"
 
 ::WindowsPowershell
-xcopy "C:\Users\plr\Documents\GIT-REPO\windows-dotfiles\WindowsPowerShell\" "C:\Users\EDU\Documents\WindowsPowerShell\"
+xcopy "C:\Users\PLR\Documents\MY-GIT-REPO\windows-dotfiles\WindowsPowerShell\" "C:\Users\PLR\Documents\WindowsPowerShell\"
 
 ::Windows Terminal
-xcopy /y "C:\Users\plr\Documents\GIT-REPO\windows-dotfiles\Windows-Terminal-Settings\settings.json" "C:\Users\plr\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+xcopy /y "C:\Users\PLR\Documents\MY-GIT-REPO\windows-dotfiles\Windows-Terminal-Settings\settings.json" "C:\Users\PLR\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 
 ::Geany
-xcopy /y /E "C:\Users\plr\Documents\GIT-REPO\windows-dotfiles\geany" "C:\Users\plr\AppData\Roaming\geany\"
+xcopy /y /E "C:\Users\PLR\Documents\MY-GIT-REPO\windows-dotfiles\geany" "C:\Users\PLR\AppData\Roaming\geany\"
 
 ::Espanso
-xcopy /y /E "C:\Users\plr\Documents\GIT-REPO\windows-dotfiles\espanso" "C:\Users\plr\AppData\Roaming\espanso\"
+xcopy /y /E "C:\Users\PLR\Documents\MY-GIT-REPO\windows-dotfiles\espanso" "C:\Users\PLR\AppData\Roaming\espanso\"
 
 ::FolderIco
-xcopy /y /E "C:\Users\plr\Documents\MY-GIT-REPO\windows-dotfiles\FolderIco\" "C:\ProgramData\Teorex\"
+xcopy /y /E "C:\Users\PLR\Documents\MY-GIT-REPO\windows-dotfiles\FolderIco\" "C:\ProgramData\Teorex\"
 
 ::Easy Worship 7
-::xcopy /y /E "C:\Users\plr\Documents\GIT-REPO\windows-dotfiles\easyworship7\programdata\*" "C:\ProgramData\Softouch\"
-::xcopy /y /E "C:\Users\plr\Documents\GIT-REPO\windows-dotfiles\easyworship7\roaming\*" "C:\Users\plr\AppData\Roaming\Softouch\"
+::xcopy /y /E "C:\Users\PLR\Documents\MY-GIT-REPO\windows-dotfiles\easyworship7\programdata\*" "C:\ProgramData\Softouch\"
+::xcopy /y /E "C:\Users\PLR\Documents\MY-GIT-REPO\windows-dotfiles\easyworship7\roaming\*" "C:\Users\PLR\AppData\Roaming\Softouch\"
 
