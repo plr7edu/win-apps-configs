921524cd36de2b0cdfd6a3153ccbb14be962682b
plr7edu <104339100+plr7edu@users.noreply.github.com>
2023-07-11 21:53:00 +0530

    Repo Update: 2023-07-11 21:52:46

--- syncthing/syncthing-fileserver-restore.bat
+++ syncthing/syncthing-fileserver-restore.bat
@@ -1,5 +1,6 @@
 ::Syncthing File Server (Restore)
-cd %LOCALAPPDATA%\Syncthing\
-del /Q * & rd /s /q . 2>nul
-xcopy /y /E "%HOMEPATH%\Documents\MY-GIT-REPO\windows-dotfiles\syncthing\Syncthing\*" %LOCALAPPDATA%\Syncthing\"
+cd %LOCALAPPDATA%
+md Syncthing
+
+xcopy /y /E "%HOMEPATH%\Documents\MY-GIT-REPO\windows-dotfiles\syncthing\Syncthing\*" "%LOCALAPPDATA%\Syncthing\"
 
