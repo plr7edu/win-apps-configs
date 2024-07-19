62f681070c557f8371fac2f640e85ef7ecfd0f65
plr7edu <plr7.edu@outlook.com>
2024-07-19 19:27:21 +0530

    Repo Update: 2024-07-19 19:27:20

---
+++ win-dotfiles-backup.ps1
@@ -0,0 +1,27 @@
+
+# Alaritty
+Copy-Item -Path "$env:APPDATA\alacritty\*" -Destination "$HOME\Documents\Windows-Git-Repos\windows-dotfiles\alacritty\" -Recurse -Force
+
+# Espanso
+Copy-Item -Path "$env:APPDATA\espanso\*" -Destination "$HOME\Documents\Windows-Git-Repos\windows-dotfiles\espanso" -Recurse -Force
+
+# JPEGView
+Copy-Item -Path "$env:APPDATA\JPEGView\*" -Destination "$HOME\Documents\Windows-Git-Repos\windows-dotfiles\jpegview\JPEGView\" -Recurse -Force
+
+# WindowsPowershell
+Copy-Item -Path "$HOME\Documents\PowerShell\*" -Destination "$HOME\Documents\Windows-Git-Repos\windows-dotfiles\WindowsPowerShell\" -Recurse -Force
+
+# Windows Terminal
+Copy-Item -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\*" -Destination "$HOME\Documents\Windows-Git-Repos\windows-dotfiles\Windows-Terminal-Settings\" -Recurse -Force
+
+$timestamp = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+$filePath = ".\$timestamp.txt"
+
+# Create the file
+New-Item -Path $filePath -ItemType "file"
+
+# Add date and time to the file content
+"File created on: $timestamp" | Out-File -Append -FilePath $filePath
+
+Read-Host -Prompt "Press any key to continue"
+
---
+++ win-dotfiles-restore.ps1
@@ -0,0 +1,29 @@
+
+# Alaritty
+Copy-Item -Path "$HOME\Documents\Windows-Git-Repos\windows-dotfiles\alacritty\*" -Destination "$env:APPDATA\alacritty\" -Recurse -Force
+
+# Starship
+Copy-Item -Path "$HOME\Documents\Windows-Git-Repos\windows-dotfiles\starship\" -Destination "$env:APPDATA\starship\" -Recurse -Force
+
+# WindowsPowershell 5
+Copy-Item -Path "$HOME\Documents\Windows-Git-Repos\windows-dotfiles\WindowsPowerShell\*" -Destination "$HOME\Documents\WindowsPowerShell\" -Recurse -Force
+
+# WindowsPowershell 7
+Copy-Item -Path "$HOME\Documents\Windows-Git-Repos\windows-dotfiles\WindowsPowerShell\*" -Destination "$HOME\Documents\PowerShell\" -Recurse -Force
+
+# Windows Terminal
+Copy-Item -Path "$HOME\Documents\Windows-Git-Repos\windows-dotfiles\Windows-Terminal-Settings\*" -Destination "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" -Recurse -Force
+
+# Espanso
+Copy-Item -Path "$HOME\Documents\Windows-Git-Repos\windows-dotfiles\espanso" -Destination "$env:APPDATA" -Recurse -Force
+
+# Jdonwloader2
+Copy-Item -Path "$HOME\Documents\Windows-Git-Repos\windows-dotfiles\jdownloader2\laf" -Destination "$env:LOCALAPPDATA\JDownloader 2.0\cfg\" -Recurse -Force
+
+# JPEGView
+Copy-Item -Path "$HOME\Documents\Windows-Git-Repos\windows-dotfiles\jpegview\JPEGView\" -Destination "$env:APPDATA\JPEGView\" -Recurse -Force
+
+# Typora (Themes)
+Copy-Item -Path "$HOME\Documents\Windows-Git-Repos\windows-dotfiles\typora\themes\*" -Destination "$env:APPDATA\Typora\themes\" -Recurse -Force
+
+Read-Host -Prompt "Press any key to continue"
